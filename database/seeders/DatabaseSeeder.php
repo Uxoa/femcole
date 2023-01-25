8b1a2f100502cc51aefa3592589b643c250046a5
EstherAma <estherdlz@hotmail.com>
2023-01-25 13:32:14 +0100

    migrations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Student;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +16,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
+        
+        Student::factory()->create([
+            'studentName' => 'Adriana',
+            'studentSurname1' => 'Aguilar',
+            'studentSurname2'=> 'Ruiz',
+            'studentEmail' => 'adriana@gmail.com',
+            'studentPassword' => 'password',
+            'studentImage' => 'https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59094623-stock-illustration-female-avatar-woman.jpg',
+        ]);
+        
+        Student::factory()->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
